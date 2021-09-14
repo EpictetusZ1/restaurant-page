87254b1cb618dd2e48e8024d94f0d47aba4d895c
J Heaton <jackrobertheaton@gmail.com>
2021-09-13 21:19:11 -0400

    Add component, display hero image

--- src/componentBasePage/index.js
+++ src/componentBasePage/index.js
@@ -1,3 +1,36 @@
-import "./styles/style.css";
+import "./styles/style.css"
+import avoBowl from "./assets/images/avoBowl.png"
 
-console.log("Test log from index.js, to check Webpack functionality")
+
+const Hero = (() => {
+
+    const contentMain = document.getElementById("content")
+    const bowlImg = document.createElement("img")
+    bowlImg.src=avoBowl
+    bowlImg.classList.add("avo-bowl")
+
+    const div = document.createElement("div")
+
+    const displayHero = () => {
+        let main = div.cloneNode()
+        main.classList.add("main")
+        contentMain.appendChild(main)
+    }
+
+    const displayBowlCont = () => {
+        const main = document.querySelector(".main")
+        let bowlCont = div.cloneNode()
+        bowlCont.classList.add("bowl-container")
+        main.appendChild(bowlCont)
+        bowlCont.appendChild(bowlImg)
+    }
+
+    return {
+        displayHero,
+        displayBowlCont
+    }
+})()
+
+
+Hero.displayHero()
+Hero.displayBowlCont()
