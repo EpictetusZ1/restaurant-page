bf612f535bf010019c197032577231ec520213b6
J Heaton <jackrobertheaton@gmail.com>
2021-09-14 00:39:54 -0400

    Refactor load hero, add text, styles

--- src/componentBasePage/index.js
+++ src/componentBasePage/index.js
@@ -1,36 +1,8 @@
 import "./styles/style.css"
-import avoBowl from "./assets/images/avoBowl.png"
+import Hero from "./loadHero"
 
+Hero.showAvocado().displayHero()
+Hero.showAvocado().displayBackDrop().showGreen()
+Hero.showAvocado().displayBackDrop().showPink()
 
-const Hero = (() => {
-
-    const contentMain = document.getElementById("content")
-    const bowlImg = document.createElement("img")
-    bowlImg.src=avoBowl
-    bowlImg.classList.add("avo-bowl")
-
-    const div = document.createElement("div")
-
-    const displayHero = () => {
-        let main = div.cloneNode()
-        main.classList.add("main")
-        contentMain.appendChild(main)
-    }
-
-    const displayBowlCont = () => {
-        const main = document.querySelector(".main")
-        let bowlCont = div.cloneNode()
-        bowlCont.classList.add("bowl-container")
-        main.appendChild(bowlCont)
-        bowlCont.appendChild(bowlImg)
-    }
-
-    return {
-        displayHero,
-        displayBowlCont
-    }
-})()
-
-
-Hero.displayHero()
-Hero.displayBowlCont()
+Hero.showAvocado().showText().pinkSide()
