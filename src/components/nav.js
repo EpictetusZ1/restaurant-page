2f75cbe83d22ff6c42904fd5bb0bf73e4ae8b067
J Heaton <jackrobertheaton@gmail.com>
2021-09-15 17:40:09 -0400

    Add functionality to nav, tabular browsing.

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,5 @@
 import Help from "../helpers/helpers";
+import Update from "./updatePage";
 
 
 const NavBar = (() =>{
@@ -10,6 +11,21 @@ const NavBar = (() =>{
             id: "home"
         }, "Home")
         nav.appendChild(homeBtn)
+        homeBtn.addEventListener("click", () => Update.page(3))
+
+        let contactBtn = Help.makeEl("div", {
+            class: "nav-item",
+            id: "contact"
+        }, "Contact")
+        nav.appendChild(contactBtn)
+        contactBtn.addEventListener("click", () => Update.page(2))
+
+        let menu = Help.makeEl("div", {
+            class: "nav-item",
+            id: "menu"
+        }, "Menu")
+        nav.appendChild(menu)
+        menu.addEventListener("click", () => Update.page(1))
     }
 
     return {
