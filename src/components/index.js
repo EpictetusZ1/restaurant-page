7dcf9c68bb4d7c33a00098b1f90355cf8b958e10
J Heaton <jackrobertheaton@gmail.com>
2021-09-15 20:24:24 -0400

    Re-add styles, rolled back too far

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,24 @@
 import "../styles/style.css"
 import Update from "./updatePage";
 import Hero from "./loadHero"
+
 import NavBar from "./nav";
 
+NavBar.addNavItems()
 
 const content = document.getElementById("content")
 
-const initContent = () => {
-    // const getNav = () =>
-    NavBar.addNavItems()
+const loadInitContent = () => {
     Hero.showAvocado(content)
+
     const menu = document.getElementById("view-menu")
     const contact = document.getElementById("contact-btn")
+
     menu.addEventListener("click", () => Update.page(1))
     contact.addEventListener("click", () => Update.page(2))
 }
 
-window.onload = () => initContent()
+window.onload = () => loadInitContent()
 
 
 
