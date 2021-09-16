3a42c325729d895247e0d491c08e248c716d25e8
J Heaton <jackrobertheaton@gmail.com>
2021-09-15 20:15:00 -0400

    Add hover effect to nav, add package-lock.json to git

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,22 @@
 import "../styles/style.css"
 import Update from "./updatePage";
 import Hero from "./loadHero"
-
 import NavBar from "./nav";
 
-NavBar.addNavItems()
 
 const content = document.getElementById("content")
 
-const loadInitContent = () => {
+const initContent = () => {
+    // const getNav = () =>
+    NavBar.addNavItems()
     Hero.showAvocado(content)
-
     const menu = document.getElementById("view-menu")
     const contact = document.getElementById("contact-btn")
-
     menu.addEventListener("click", () => Update.page(1))
     contact.addEventListener("click", () => Update.page(2))
 }
 
-window.onload = () => loadInitContent()
+window.onload = () => initContent()
 
 
 
